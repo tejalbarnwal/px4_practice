cedd42840161457d3b3942c96a4ca3e9e6da8c2d
tejalbarnwal <tejalbarnwal@gmail.com>
2021-06-24 14:24:30 +0530

    raster done

--- scripts/raster.py
+++ scripts/raster.py
@@ -15,30 +15,40 @@ class raster_setpoints_control():
         rospy.init_node("raster_scan_node")
 
         # subscriber to /mavros/state
-        rospy.Subscriber("/mavros/state" ,State, self.state_callback , queue_size = 1)
+        rospy.Subscriber("/mavros/local_position/pose" , PoseStamped, self.pose_callback , queue_size = 1)
 
         self.teju_setpoint = rospy.Publisher("/teju/give_setpoints" , String , queue_size = 1)
 
         self.raster_points = [ [0,10], [2,0], [0,-10], [2,0] ]
         self.estimate_pose = [0,0]
 
-    def odom_callback(self , msg):
+    def pose_callback(self , msg):
         self.pose_x = msg.pose.position.x
         self.pose_y = msg.pose.position.y
 
 
     def action(self , rounds):
         
-        for i in rounds :
+        for i in range(rounds) :
             for j in self.raster_points:
-                    if abs(self.pose_x - self.estimate_pose) <= 0.01 and abs(self.pose_y-self.estimate_pose[1]) <= 0.01:
-                        
-                        self.teju_setpoint.publish(f"{j[0]} {j[1]} 0")
+                    print("x" , abs(self.pose_x - self.estimate_pose[0]))
+                    print("y" , abs(self.pose_y-self.estimate_pose[1]))
+                    print("esimate_y" , self.estimate_pose[1])
+                    print("estimate_x" , self.estimate_pose[0])
+
+                    if abs(self.pose_x - self.estimate_pose[0]) <= 0.1 and abs(self.pose_y-self.estimate_pose[1]) <= 0.1:
+                        yo = str(j[0]) + " " + str(j[1]) + " " + "0"
+                        print("yo" , yo)
+                        self.teju_setpoint.publish(yo)
                         self.estimate_pose[0] = self.estimate_pose[0] + j[0]
                         self.estimate_pose[1] = self.estimate_pose[1] + j[1]
-                    else:
-                        while abs(self.pose_x - self.estimate_pose) >= 0.01 and abs(self.pose_y-self.estimate_pose[1]) >= 0.01:
+                        time.sleep(10)
+                        while abs(self.pose_x - self.estimate_pose[0]) > 0.1 and abs(self.pose_y-self.estimate_pose[1]) > 0.1:
+                            print("x1" , abs(self.pose_x - self.estimate_pose[0]))
+                            print("y1" , abs(self.pose_y-self.estimate_pose[1]))
                             pass
+                    else:
+                        print("ohh noooo")
 
 
 if __name__=="__main__":
